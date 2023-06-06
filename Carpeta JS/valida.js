954c46b2574eb4a091cecc9b6fd7759ed3959757
LorenaRis <77359581+LorenaRis@users.noreply.github.com>
2023-06-06 17:09:07 -0300

    validación con modificaciones

--- Carpeta JS/valida.js
+++ Carpeta JS/valida.js
@@ -4,22 +4,32 @@
 
 function validarFormulario(evento) {
   evento.preventDefault();
-  var nombre = document.getElementById('nombre').value;
-  if (nombre.length < 4) {
-    alert('escribe un nombre valido')
+  let nombre = document.getElementById('nombre').value
+  let apellido =document.getElementById ('apellido').value
+  let telefono =document.getElementById ('telefono').value
+  let tlinea =document.getElementById ('tlinea').value
+  let horac =document.getElementById ('horac')
+  let email =document.getElementById ('email').value
+  console.log(nombre,apellido,telefono,tlinea,horac,email)
+  if (nombre.length < 4||nombre [0] ==" ") {
+    alert('Escribe un nombre válido')
     document.getElementById('nombre').focus();
     return 0;
   }
-  if (document.getElementById('horac').selectedIndex == 0) {
+  if (horac.selectedIndex == 0) {
     alert("Seleccione un rango horario")
     document.getElementById('horac').focus();
     return 0;
   }
+  if (!nombre || !apellido || !telefono|| !email){
+    alert("Se deben completar todos los datos de contacto")
+  }
+
   this.submit();
 }
 function validarCorreo(email) {
-  var expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-  var esValido = expReg.test(email);
+  let expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  let esValido = expReg.test(email);
   if (esValido == false) {
     alert("Ingrese un E-mail valido")
     document.getElementById("email").value = ""
